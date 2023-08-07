12b956a0a9ca3d8b4b321d557a110c47ba59aa4a
Joel Rhea <joelw.rhea@gmail.com>
2023-08-06 21:20:23 -0400

    Doing a second commit, first didn't go thru

--- cipher.py
+++ cipher.py
@@ -2,12 +2,12 @@
 user_input = input("Please enter a sentence: ")
 shift_amount = 5
 
-# Create translation table for wraparound
+
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 shifted_alphabet = alphabet[shift_amount:] + alphabet[:shift_amount]
 trans_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
 
-# Encrypt the user input using Caesar cipher
+
 cipher_text = user_input.translate(trans_table)
 
 print("Cipher text:", cipher_text)
